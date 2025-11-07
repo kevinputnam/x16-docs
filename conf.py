a4911c932860c7578119e60fcbf9f1835945328a
Kevin Putnam <kevin.putnam@gmail.com>
2025-11-06 22:31:42 -0800

    Switch to RTD theme for improved navigation
    Signed-off-by: Kevin Putnam <kevin.putnam@gmail.com>

--- conf.py
+++ conf.py
@@ -23,7 +23,7 @@ root_doc = 'README'
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'pydata_sphinx_theme'
+html_theme = 'sphinx_rtd_theme'
 html_theme_options = {
       "logo": {
         "text": "Commander X16"
